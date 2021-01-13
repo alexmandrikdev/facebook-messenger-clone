c19ba03055038a78be6d160b4843bd396c6a3551
MnDAlex <alexvok12@gmail.com>
2021-01-13 12:20:17 +0100

    Set up Vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
-require('./bootstrap');
+require("./bootstrap");
+
+import Vue from "vue";
+
+import App from "./components/App.vue";
+
+const app = new Vue({
+    el: "#app",
+    components: { App },
+});
