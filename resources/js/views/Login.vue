977d4965bc1aa056c9c0719f0f2695fa48ba17ef
MnDAlex <alexvok12@gmail.com>
2021-01-22 13:48:49 +0100

    Merge AuthLayout into Login view

--- resources/js/views/Login.vue
+++ resources/js/views/Login.vue
@@ -1,63 +1,192 @@
 <template>
     <div>
-        <auth-layout>
-            <form>
-                <input
-                    v-model="formData.email"
-                    type="text"
-                    placeholder="Email"
-                />
-                <input
-                    v-model="formData.password"
-                    type="password"
-                    placeholder="Password"
-                />
-                <div class="flex">
-                    <button type="submit">Log In</button>
+        <header
+            :class="{
+                'border-b border-black border-opacity-10 fixed': !isAtTopOfPage,
+            }"
+            class="px-8 xl:px-0 h-24 w-full top-0 bg-clip-padding z-50 bg-white bg-opacity-98"
+        >
+            <div class="container mx-auto h-full flex items-center">
+                <router-link to="/">
+                    <img
+                        src="/images/logo.png"
+                        alt="messenger logo"
+                        class="h-10"
+                    />
+                </router-link>
+                <div class="ml-auto hidden md:flex font-semibold">
                     <router-link
-                        class="text-blue-500 underline font-semibold align-middle my-auto ml-5 text-sm"
-                        to="#"
-                        >Forgot your password?</router-link
+                        v-for="(menuItem, index) in menuItems.filter(
+                            menuItem => menuItem.show,
+                        )"
+                        :key="index"
+                        class="cursor-pointer relative ml-8"
+                        :to="menuItem.to"
                     >
+                        {{ menuItem.label }}
+                    </router-link>
                 </div>
-                <div class="flex mt-6 mb-5">
-                    <div class="relative">
-                        <input
-                            v-model="formData.keepMeSignedIn"
-                            class="opacity-0"
-                            type="checkbox"
-                        />
+                <base-hamburger-button
+                    class="md:hidden ml-auto"
+                    :is-open="isMenuOpen"
+                    @click="onMenuButtonClicked"
+                />
+            </div>
+        </header>
+
+        <auth-layout-mobile-menu
+            v-if="isMenuOpen"
+            class="md:hidden mt-24"
+            :menu-items="menuItems"
+        />
+
+        <template v-else>
+            <!-- MAIN SECTION -->
+            <section
+                class="container mx-auto flex flex-col lg:flex-row px-8 xl:px-0"
+                :class="[
+                    !isAtTopOfPage
+                        ? 'mt-48 md:mt-32 lg:mt-64'
+                        : 'mt-24 md:mt-8 lg:mt-40',
+                ]"
+            >
+                <div class="lg:w-1/2">
+                    <div
+                        class="max-w-0.763889xl mx-auto lg:mx-0 text-left md:text-center lg:text-left"
+                    >
                         <div
-                            class="w-4 h-4 border border-gray-300 rounded absolute top-1/2 right-0 transform -translate-y-1/2 -z-10"
-                            :class="{
-                                'bg-blue-500 border-blue-500':
-                                    formData.keepMeSignedIn,
-                            }"
+                            id="hang-out"
+                            class="text-5.833 lg:text-7xl bg-clip-text text-transparent font-semibold tracking-tighter pb-3 leading-13 lg:leading-21.25"
                         >
-                            <div
-                                v-if="formData.keepMeSignedIn"
-                                class="absolute h-3 w-1.2 border-b border-r border-white transform rotate-45 top-1/2 left-1/2 -translate-x-1/2 -translate-y-55%"
-                            ></div>
+                            Hang out <br class="lg:hidden" />
+                            anytime, anywhere
                         </div>
+
+                        <div class="text-gray-600 text-lg mt-3 mb-20 lg:mb-10">
+                            Messenger makes it easy and fun to stay close to
+                            your favorite people.
+                        </div>
+
+                        <form>
+                            <input
+                                v-model="formData.email"
+                                type="text"
+                                placeholder="Email"
+                            />
+                            <input
+                                v-model="formData.password"
+                                type="password"
+                                placeholder="Password"
+                            />
+                            <div class="flex">
+                                <button type="submit">Log In</button>
+                                <router-link
+                                    class="text-blue-500 underline font-semibold align-middle my-auto ml-5 text-sm"
+                                    to="#"
+                                    >Forgot your password?</router-link
+                                >
+                            </div>
+                            <div class="flex mt-6 mb-5">
+                                <div class="relative">
+                                    <input
+                                        v-model="formData.keepMeSignedIn"
+                                        class="opacity-0"
+                                        type="checkbox"
+                                    />
+                                    <div
+                                        class="w-4 h-4 border border-gray-300 rounded absolute top-1/2 right-0 transform -translate-y-1/2 -z-10"
+                                        :class="{
+                                            'bg-blue-500 border-blue-500':
+                                                formData.keepMeSignedIn,
+                                        }"
+                                    >
+                                        <div
+                                            v-if="formData.keepMeSignedIn"
+                                            class="absolute h-3 w-1.2 border-b border-r border-white transform rotate-45 top-1/2 left-1/2 -translate-x-1/2 -translate-y-55%"
+                                        ></div>
+                                    </div>
+                                </div>
+                                <label
+                                    class="ml-3 my-auto text-sm text-gray-600"
+                                    >Keep me signed in</label
+                                >
+                            </div>
+                        </form>
                     </div>
-                    <label class="ml-3 my-auto text-sm text-gray-600"
-                        >Keep me signed in</label
+                </div>
+                <div
+                    class="max-w-0.91667xl lg:max-w-none md:w-110 lg:w-1/2 mx-auto lg:mx-0 lg:flex lg:items-center"
+                >
+                    <img
+                        class="transform lg:translate-x-8 lg:-translate-y-12"
+                        src="/images/auth-layout-image.png"
+                    />
+                </div>
+            </section>
+
+            <footer
+                class="text-center text-sm mt-16 py-5 px-7.5 lg:flex lg:justify-center lg:items-center"
+            >
+                <strong class="lg:pr-6">© Facebook 2021.</strong>
+                <div class="flex justify-center mt-5 mb-2.5 lg:m-0 lg:px-6">
+                    <a
+                        class="px-6"
+                        target="__blank"
+                        href="https://www.facebook.com/policy.php"
+                        >Data Policy</a
+                    >
+                    <a
+                        class="px-6"
+                        target="__blank"
+                        href="https://www.facebook.com/policies/?ref=pf"
+                        >Terms</a
                     >
                 </div>
-            </form>
-        </auth-layout>
+                <img
+                    class="mx-auto lg:mx-0 max-w-1.041667xl h-full"
+                    src="/images/from-facebook.jpg"
+                    alt="From Facebook"
+                />
+            </footer>
+        </template>
     </div>
 </template>
 
 <script>
-import AuthLayout from '../components/AuthLayout.vue';
-
+import AuthLayoutMobileMenu from '../components/AuthLayoutMobileMenu.vue';
+import BaseHamburgerButton from '../components/BaseHamburgerButton.vue';
 export default {
     components: {
-        AuthLayout,
+        BaseHamburgerButton,
+        AuthLayoutMobileMenu,
     },
     data() {
         return {
+            topOfPage: 35,
+            isAtTopOfPage: true,
+            isMenuOpen: false,
+            menuItems: [
+                {
+                    show: true,
+                    to: '/#',
+                    label: 'Rooms',
+                },
+                {
+                    show: true,
+                    to: '/#',
+                    label: 'Features',
+                },
+                {
+                    show: true,
+                    to: '/#',
+                    label: 'Privacy & Safety',
+                },
+                {
+                    show: true,
+                    to: '/#',
+                    label: 'For Developers',
+                },
+            ],
             formData: {
                 email: '',
                 password: '',
@@ -65,7 +194,48 @@ export default {
             },
         };
     },
+    beforeMount() {
+        window.addEventListener('scroll', this.handleScroll);
+    },
+    methods: {
+        handleScroll() {
+            this.isAtTopOfPage = window.pageYOffset <= this.topOfPage;
+        },
+        onMenuButtonClicked() {
+            this.isMenuOpen = !this.isMenuOpen;
+        },
+    },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+#hang-out {
+    background-image: linear-gradient(
+        83.84deg,
+        #0088ff -6.87%,
+        #a033ff 26.54%,
+        #ff5c87 58.58%
+    );
+}
+
+form {
+    @apply lg:w-80;
+}
+
+form input[type='text'],
+form input[type='password'] {
+    @apply bg-gray-100 h-9 mb-3 py-2 px-4 text-lg w-full rounded-1.25xl;
+}
+
+form button[type='submit'] {
+    @apply bg-blue-500 font-bold rounded-3xl mt-5 mb-3 text-white px-5 py-2.5 hover:bg-blue-600 transition duration-200;
+}
+
+form input.invalid {
+    @apply border border-red-500;
+}
+
+form .invalid-feedback {
+    @apply text-red-500 text-sm mb-3 -mt-3;
+}
+</style>
--- resources/js/views/Register.vue
+++ resources/js/views/Register.vue
@@ -1,142 +1,135 @@
 <template>
     <div>
-        <auth-layout>
-            <form>
-                <input
-                    v-model.trim.lazy="$v.formData.email.$model"
-                    :class="{ invalid: $v.formData.email.$error }"
-                    type="text"
-                    placeholder="Email"
-                    name="email"
-                    required
-                    @blur="$v.formData.email.$touch()"
-                />
-                <div
-                    v-if="
-                        $v.formData.email.$dirty && !$v.formData.email.required
-                    "
-                    class="invalid-feedback"
-                >
-                    The email is required
-                </div>
-                <div
-                    v-if="$v.formData.email.$dirty && !$v.formData.email.email"
-                    class="invalid-feedback"
-                >
-                    The email must be a valid email
-                </div>
+        <form>
+            <input
+                v-model.trim.lazy="$v.formData.email.$model"
+                :class="{ invalid: $v.formData.email.$error }"
+                type="text"
+                placeholder="Email"
+                name="email"
+                required
+                @blur="$v.formData.email.$touch()"
+            />
+            <div
+                v-if="$v.formData.email.$dirty && !$v.formData.email.required"
+                class="invalid-feedback"
+            >
+                The email is required
+            </div>
+            <div
+                v-if="$v.formData.email.$dirty && !$v.formData.email.email"
+                class="invalid-feedback"
+            >
+                The email must be a valid email
+            </div>
 
-                <input
-                    v-model.trim="formData.password"
-                    :class="{ invalid: $v.formData.password.$error }"
-                    type="password"
-                    placeholder="Password"
-                    name="password"
-                    required
-                    @blur="$v.formData.password.$touch()"
-                />
-                <div
-                    v-if="
-                        $v.formData.password.$dirty &&
-                            !$v.formData.password.required
-                    "
-                    class="invalid-feedback"
-                >
-                    The password is required
-                </div>
-                <div
-                    v-if="
-                        $v.formData.password.$dirty &&
-                            !$v.formData.password.minLength
-                    "
-                    class="invalid-feedback"
-                >
-                    The password length must be at least
-                    {{ passwordMinLength }} character
-                </div>
+            <input
+                v-model.trim="formData.password"
+                :class="{ invalid: $v.formData.password.$error }"
+                type="password"
+                placeholder="Password"
+                name="password"
+                required
+                @blur="$v.formData.password.$touch()"
+            />
+            <div
+                v-if="
+                    $v.formData.password.$dirty &&
+                        !$v.formData.password.required
+                "
+                class="invalid-feedback"
+            >
+                The password is required
+            </div>
+            <div
+                v-if="
+                    $v.formData.password.$dirty &&
+                        !$v.formData.password.minLength
+                "
+                class="invalid-feedback"
+            >
+                The password length must be at least
+                {{ passwordMinLength }} character
+            </div>
 
-                <input
-                    v-model.trim="formData.passwordConfirmation"
-                    :class="{
-                        invalid: $v.formData.passwordConfirmation.$error,
-                    }"
-                    type="password"
-                    placeholder="Password confirmation"
-                    name="passwordConfirmation"
-                    required
-                    @blur="$v.formData.passwordConfirmation.$touch()"
-                />
-                <div
-                    v-if="
-                        $v.formData.passwordConfirmation.$dirty &&
-                            !$v.formData.passwordConfirmation.required
-                    "
-                    class="invalid-feedback"
-                >
-                    The password confirmation is required
-                </div>
-                <div
-                    v-if="
-                        $v.formData.passwordConfirmation.$dirty &&
-                            !$v.formData.passwordConfirmation.sameAsPassword
-                    "
-                    class="invalid-feedback"
-                >
-                    The password confirmation must be the same as the password
-                </div>
+            <input
+                v-model.trim="formData.passwordConfirmation"
+                :class="{
+                    invalid: $v.formData.passwordConfirmation.$error,
+                }"
+                type="password"
+                placeholder="Password confirmation"
+                name="passwordConfirmation"
+                required
+                @blur="$v.formData.passwordConfirmation.$touch()"
+            />
+            <div
+                v-if="
+                    $v.formData.passwordConfirmation.$dirty &&
+                        !$v.formData.passwordConfirmation.required
+                "
+                class="invalid-feedback"
+            >
+                The password confirmation is required
+            </div>
+            <div
+                v-if="
+                    $v.formData.passwordConfirmation.$dirty &&
+                        !$v.formData.passwordConfirmation.sameAsPassword
+                "
+                class="invalid-feedback"
+            >
+                The password confirmation must be the same as the password
+            </div>
 
-                <input
-                    v-model.trim="formData.firstName"
-                    :class="{ invalid: $v.formData.firstName.$error }"
-                    type="text"
-                    placeholder="First name"
-                    name="firstName"
-                    required
-                    @blur="$v.formData.firstName.$touch()"
-                />
-                <div
-                    v-if="
-                        $v.formData.firstName.$dirty &&
-                            !$v.formData.firstName.required
-                    "
-                    class="invalid-feedback"
-                >
-                    The first name is required
-                </div>
+            <input
+                v-model.trim="formData.firstName"
+                :class="{ invalid: $v.formData.firstName.$error }"
+                type="text"
+                placeholder="First name"
+                name="firstName"
+                required
+                @blur="$v.formData.firstName.$touch()"
+            />
+            <div
+                v-if="
+                    $v.formData.firstName.$dirty &&
+                        !$v.formData.firstName.required
+                "
+                class="invalid-feedback"
+            >
+                The first name is required
+            </div>
 
-                <input
-                    v-model.trim="formData.lastName"
-                    :class="{ invalid: $v.formData.lastName.$error }"
-                    type="text"
-                    placeholder="Last name"
-                    name="lastName"
-                    required
-                    @blur="$v.formData.lastName.$touch()"
-                />
-                <div
-                    v-if="
-                        $v.formData.lastName.$dirty &&
-                            !$v.formData.lastName.required
-                    "
-                    class="invalid-feedback"
-                >
-                    The last name is required
-                </div>
+            <input
+                v-model.trim="formData.lastName"
+                :class="{ invalid: $v.formData.lastName.$error }"
+                type="text"
+                placeholder="Last name"
+                name="lastName"
+                required
+                @blur="$v.formData.lastName.$touch()"
+            />
+            <div
+                v-if="
+                    $v.formData.lastName.$dirty &&
+                        !$v.formData.lastName.required
+                "
+                class="invalid-feedback"
+            >
+                The last name is required
+            </div>
 
-                <button type="submit">Register</button>
-            </form>
-        </auth-layout>
+            <button type="submit">Register</button>
+        </form>
     </div>
 </template>
 
 <script>
-import AuthLayout from '../components/AuthLayout.vue';
 import { required, minLength, sameAs, email } from 'vuelidate/lib/validators';
 
 export default {
-    components: {
-        AuthLayout,
-    },
+    components: {},
     data() {
         return {
             formData: {
