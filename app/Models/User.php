b862db5049e1f663f8914c32df34266a625d8968
MnDAlex <alexvok12@gmail.com>
2021-01-25 16:49:43 +0100

    Set User fields

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,9 +17,10 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name',
         'email',
         'password',
+        'first_name',
+        'last_name'
     ];
 
     /**
