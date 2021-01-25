5e85c6a129894274ddaf53e565f2130e5f128f3b
MnDAlex <alexvok12@gmail.com>
2021-01-25 11:56:59 +0100

    Change title

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <title>MnD Messenger</title>
+    <title>Messenger</title>
 </head>
 <body>
     <div id="app">
