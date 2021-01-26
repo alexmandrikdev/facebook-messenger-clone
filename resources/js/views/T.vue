1a2661e1d47f0a9c77f2e06a6fd697b4afa55c21
MnDAlex <alexvok12@gmail.com>
2021-01-26 14:27:43 +0100

    Implement basic layout in T view

--- resources/js/views/T.vue
+++ resources/js/views/T.vue
@@ -1,6 +1,14 @@
 <template>
-    <div>
-        Index
+    <div class="flex flex-row h-screen">
+        <div class="w-90"></div>
+        <div
+            class="w-screen-minus-w-90 flex flex-row border-l border-black border-opacity-10"
+        >
+            <div class="flex-1"></div>
+            <div
+                class="w-4/12 max-w-94.75 min-w-62.25 border-l border-black border-opacity-10"
+            ></div>
+        </div>
     </div>
 </template>
 
@@ -14,4 +22,8 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.w-screen-minus-w-90 {
+    width: calc(100vw - 22.5rem);
+}
+</style>
