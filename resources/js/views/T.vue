f4763a1684559290a3a62ca0dfc1fd746bdfbbf4
MnDAlex <alexvok12@gmail.com>
2021-01-26 17:20:26 +0100

    Refactor the toggleInfoMenu button

--- resources/js/views/T.vue
+++ resources/js/views/T.vue
@@ -10,26 +10,25 @@
                     <div class="">Name</div>
                     <div class="flex">
                         <div
-                            class="relative w-7 h-7 m-2.5 cursor-pointer"
+                            class="w-7 h-7 m-2.5 cursor-pointer"
                             @click="toggleInfoMenu"
                         >
-                            <div
-                                class="w-7 h-7 rounded-full bg-blue-100 absolute top-0 left-0"
-                            ></div>
-                            <svg
-                                class="absolute top-0 left-0"
-                                role="presentation"
-                                height="28px"
-                                width="28px"
-                                fill="#0099FF"
-                                viewBox="0 0 36 36"
-                            >
-                                <path
-                                    clip-rule="evenodd"
-                                    d="M18 29c6.075 0 11-4.925 11-11S24.075 7 18 7 7 11.925 7 18s4.925 11 11 11zm1.75-16.25a1.75 1.75 0 11-3.5 0 1.75 1.75 0 013.5 0zM16.5 17.5a1 1 0 011-1h1a1 1 0 011 1V24a1 1 0 01-1 1h-1a1 1 0 01-1-1v-6.5z"
-                                    fill-rule="evenodd"
-                                ></path>
-                            </svg>
+                            <div class="w-7 h-7 rounded-full bg-blue-100">
+                                <svg
+                                    class=""
+                                    role="presentation"
+                                    height="28px"
+                                    width="28px"
+                                    fill="#0099FF"
+                                    viewBox="0 0 36 36"
+                                >
+                                    <path
+                                        clip-rule="evenodd"
+                                        d="M18 29c6.075 0 11-4.925 11-11S24.075 7 18 7 7 11.925 7 18s4.925 11 11 11zm1.75-16.25a1.75 1.75 0 11-3.5 0 1.75 1.75 0 013.5 0zM16.5 17.5a1 1 0 011-1h1a1 1 0 011 1V24a1 1 0 01-1 1h-1a1 1 0 01-1-1v-6.5z"
+                                        fill-rule="evenodd"
+                                    ></path>
+                                </svg>
+                            </div>
                         </div>
                     </div>
                 </div>
