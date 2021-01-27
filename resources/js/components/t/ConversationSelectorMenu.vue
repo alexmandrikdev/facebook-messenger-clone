a01efa96782f44ff075072822cdc645564b37e98
MnDAlex <alexvok12@gmail.com>
2021-01-27 09:47:43 +0100

    Move header into a separate component

--- resources/js/components/t/ConversationSelectorMenu.vue
+++ resources/js/components/t/ConversationSelectorMenu.vue
@@ -1,42 +1,15 @@
 <template>
     <div class="w-full md:w-90">
-        <div class="flex mx-4 mt-5 mb-3 h-9">
-            <img
-                src="/images/profile-picture-placeholder.png"
-                alt="profile picture"
-                class="w-9 rounded-full object-contain object-center"
-            />
-
-            <div class="text-2xl font-bold ml-3">Chats</div>
-
-            <div
-                class="ml-auto w-9 bg-gray-100 rounded-full flex cursor-pointer hover:bg-gray-200"
-            >
-                <svg viewBox="0 0 36 36" class="m-auto" height="28" width="28">
-                    <path
-                        d="M12.5 18A2.25 2.25 0 118 18a2.25 2.25 0 014.5 0zm7.75 0a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zm5.5 2.25a2.25 2.25 0 100-4.5 2.25 2.25 0 000 4.5z"
-                    ></path>
-                </svg>
-            </div>
-
-            <div
-                class="ml-3 w-9 bg-gray-100 rounded-full flex cursor-pointer hover:bg-gray-200"
-            >
-                <svg viewBox="0 0 36 36" height="28" width="28" class="m-auto">
-                    <path
-                        d="M17.305 16.57a1.998 1.998 0 00-.347.467l-1.546 2.87a.5.5 0 00.678.677l2.87-1.545c.171-.093.328-.21.466-.347l8.631-8.631a1.5 1.5 0 10-2.121-2.122l-8.631 8.632z"
-                    ></path>
-                    <path
-                        d="M18 10.5a1 1 0 001-1V9a1 1 0 00-1-1h-6a4 4 0 00-4 4v12a4 4 0 004 4h12a4 4 0 004-4v-6a1 1 0 00-1-1h-.5a1 1 0 00-1 1v6a1.5 1.5 0 01-1.5 1.5H12a1.5 1.5 0 01-1.5-1.5V12a1.5 1.5 0 011.5-1.5h6z"
-                    ></path>
-                </svg>
-            </div>
-        </div>
+        <Header />
     </div>
 </template>
 
 <script>
-export default {};
+import Header from './conversation-selector-menu/Header.vue';
+
+export default {
+    components: { Header },
+};
 </script>
 
 <style scoped></style>
