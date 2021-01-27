abe4f8beaa35ba3e2da06d3959db0aa9786e7355
MnDAlex <alexvok12@gmail.com>
2021-01-27 10:13:40 +0100

    Implement click-outside Vue directive

--- resources/js/app.js
+++ resources/js/app.js
@@ -13,6 +13,22 @@ Vue.component('BaseHamburgerButton', BaseHamburgerButton);
 
 Vue.use(Vuelidate);
 
+Vue.directive('click-outside', {
+    bind: function(el, binding, vnode) {
+        el.clickOutsideEvent = function(event) {
+            // here I check that click was outside the el and his children
+            if (!(el == event.target || el.contains(event.target))) {
+                // and if it did, call method provided in attribute value
+                vnode.context[binding.expression](event);
+            }
+        };
+        document.body.addEventListener('click', el.clickOutsideEvent);
+    },
+    unbind: function(el) {
+        document.body.removeEventListener('click', el.clickOutsideEvent);
+    },
+});
+
 const apolloClient = new ApolloClient({
     uri: process.env.MIX_GRAPHQL_URL,
 });
