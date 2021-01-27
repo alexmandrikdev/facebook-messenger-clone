c867e7b23419ab9bc4771ccf694d167d4e6aac97
MnDAlex <alexvok12@gmail.com>
2021-01-27 15:48:20 +0100

    Create HeaderDropDown component

---
+++ resources/js/components/t/conversation-selector-menu/HeaderDropDown.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="pb-2 h-24 z-50 relative mt-3.75">
+        <div
+            class="w-4.5 h-4.5 bg-white transform rotate-45 absolute -top-1 left-2.25"
+        ></div>
+
+        <div class="w-85 h-full bg-white shadow rounded-lg"></div>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.shadow {
+    box-shadow: 0 12px 28px 0 rgba(0, 0, 0, 0.2), 0 2px 4px 0 rgba(0, 0, 0, 0.1),
+        inset 0 0 0 1px rgba(255, 255, 255, 0.5);
+}
+</style>
