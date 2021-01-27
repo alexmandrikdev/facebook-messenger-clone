969203142f097e909d5de04e32222a31c015bf25
MnDAlex <alexvok12@gmail.com>
2021-01-27 12:44:47 +0100

    Create Search component for ConversationSelectorMenu

--- resources/js/components/t/ConversationSelectorMenu.vue
+++ resources/js/components/t/ConversationSelectorMenu.vue
@@ -1,14 +1,16 @@
 <template>
     <div class="w-full md:w-90">
         <Header />
+        <Search />
     </div>
 </template>
 
 <script>
 import Header from './conversation-selector-menu/Header.vue';
+import Search from './conversation-selector-menu/Search.vue';
 
 export default {
-    components: { Header },
+    components: { Header, Search },
 };
 </script>
 
---
+++ resources/js/components/t/conversation-selector-menu/Search.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="mt-6 mb-5 px-4 flex">
+    </div>
+</template>
+
+<script>
+export default {
+};
+</script>
+
+<style scoped></style>
