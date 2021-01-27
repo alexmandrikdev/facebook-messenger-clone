c639df4e468a6d58ac26785bec909b965f64b741
MnDAlex <alexvok12@gmail.com>
2021-01-27 20:19:13 +0100

    Configure Apollo Client to authenticate sanctum

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,7 +5,10 @@ import router from './router';
 import store from './store/index';
 import App from './App.vue';
 import VueApollo from 'vue-apollo';
-import ApolloClient from 'apollo-boost';
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { setContext } from 'apollo-link-context';
 import Vuelidate from 'vuelidate';
 import BaseHamburgerButton from './components/BaseHamburgerButton.vue';
 
@@ -29,16 +32,41 @@ Vue.directive('click-outside', {
     },
 });
 
-const apolloClient = new ApolloClient({
-    uri: process.env.MIX_GRAPHQL_URL,
-});
-
 Vue.use(VueApollo);
 
+const authLink = setContext((_, { headers }) => {
+    let token = RegExp('XSRF-TOKEN[^;]+').exec(document.cookie);
+    token = decodeURIComponent(
+        token ? token.toString().replace(/^[^=]+./, '') : '',
+    );
+    return {
+        headers: {
+            ...headers,
+            'X-XSRF-TOKEN': token,
+        },
+    };
+});
+
+const httpLink = createHttpLink({
+    uri: '/graphql', // The graphql endpoint url
+    credentials: 'same-origin',
+});
+
+// Cache implementation
+const cache = new InMemoryCache();
+
+// Create the apollo client
+const apolloClient = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache,
+});
+
+// Create the provider for Vue
 const apolloProvider = new VueApollo({
     defaultClient: apolloClient,
 });
 
+// Initialize vue instance
 new Vue({
     router,
     store,
