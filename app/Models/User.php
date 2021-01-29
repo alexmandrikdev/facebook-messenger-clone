d47048b2a7e542d283f2c1d801f9f2d9e06a5573
MnDAlex <alexvok12@gmail.com>
2021-01-29 16:58:40 +0100

    Set hidden fields for User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -31,6 +31,11 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
         'remember_token',
+        'email_verified_at',
+        'two_factor_secret',
+        'two_factor_recovery_codes',
+        'created_at',
+        'updated_at',
     ];
 
     /**
