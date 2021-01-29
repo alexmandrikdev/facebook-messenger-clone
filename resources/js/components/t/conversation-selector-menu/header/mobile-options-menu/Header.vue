2e18dc2fb0e571590ca37ee1784161a7b3db3bd4
MnDAlex <alexvok12@gmail.com>
2021-01-29 17:48:25 +0100

    Create mobile options menu's Header component

---
+++ resources/js/components/t/conversation-selector-menu/header/mobile-options-menu/Header.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="flex items-center p-4 bg-white w-screen">
+        <base-right-arrow class="w-5 h-5" @click="$emit('click')" />
+
+        <div class="ml-7 font-semibold">Me</div>
+    </div>
+</template>
+
+<script>
+import BaseRightArrow from '../../../../BaseRightArrow.vue';
+export default {
+    components: { BaseRightArrow },
+};
+</script>
+
+<style scoped></style>
