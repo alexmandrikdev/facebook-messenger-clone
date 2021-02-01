c8895bbae98049ba23bcc904b3b881a36e764cf3
MnDAlex <alexvok12@gmail.com>
2021-02-01 19:36:09 +0100

    Display profile picture placeholder and name in Chat

--- resources/js/components/t/Chat.vue
+++ resources/js/components/t/Chat.vue
@@ -1,6 +1,29 @@
 <template>
-    <div class="flex-1 overflow-hidden">
+    <div class="flex flex-col flex-1 overflow-hidden">
         <chat-header :toggle-info-menu="toggleInfoMenu" :user="user" />
+
+        <div class="overflow-y-auto flex-1">
+            <div class="mt-5">
+                <img
+                    src="/images/profile-picture-placeholder.png"
+                    :alt="[
+                        user
+                            ? `${user.first_name} ${user.last_name}`
+                            : `${$store.state.authUser.first_name} ${$store.state.authUser.last_name}`,
+                    ]"
+                    class="w-15 h-15 mx-auto rounded-full object-cover object-center"
+                />
+
+                <div class="text-4.25 font-semibold text-center mt-1">
+                    <template v-if="user">
+                        {{ user.first_name }} {{ user.last_name }}
+                    </template>
+                    <template v-else>
+                        Facebook User
+                    </template>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
