67cfe08bbaf7caed8cfccc1abd818e14fe413310
MnDAlex <alexvok12@gmail.com>
2021-02-03 11:38:33 +0100

    Add vue-custom-scrollbar to InfoMenu

--- resources/js/components/t/InfoMenu.vue
+++ resources/js/components/t/InfoMenu.vue
@@ -1,12 +1,17 @@
 <template>
-    <div
-        class="w-4/12 max-w-94.75 min-w-62.25 border-l border-black-opacity-10"
+    <vue-custom-scrollbar
+        class="w-4/12 max-w-94.75 min-w-62.25 border-l border-black-opacity-10 overflow-y-auto flex-col"
         :class="[infoMenuToggler ? 'hidden md:flex' : 'flex md:hidden']"
-    ></div>
+    >
+    </vue-custom-scrollbar>
 </template>
 
 <script>
+import vueCustomScrollbar from 'vue-custom-scrollbar';
+import '../../../css/vue-custom-scrollbar.css';
+
 export default {
+    components: { vueCustomScrollbar },
     props: {
         infoMenuToggler: {
             type: Boolean,
